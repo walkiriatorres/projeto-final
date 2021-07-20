77d946dfa4945ad27e4a5a2b0d0baa5992b0bc75
Walkiria <walkiria_tg@hotmail.com>
2021-07-20 16:25:04 -0300

    adding departments to final project frontend

---
+++ departments/script.js
@@ -0,0 +1,43 @@
+//fazendo um fetch dos dados passando nossa rota com await
+
+async function getData(){
+	const response = await fetch("https://professor-allocation-walkiria.herokuapp.com/departments");
+	const json = await response.json();
+	return json;
+}
+async function loadTable(){
+	const data = await getData();
+	
+	for (const department of data){
+		CreateLine(department);
+	}
+}
+
+async function CreateLine(dep) {
+	let linha = document.createElement("tr");
+
+	let colunaNome = document.createElement("td");
+	colunaNome.textContent = dep.name;
+	linha.appendChild(colunaNome);
+
+	let colunaEdit = document.createElement("td");
+	let btnEdit = document.createElement("button");
+	btnEdit.textContent = Editar;
+	btnEdit.classList.add("btn");
+	btnEdit.classList.add("btn-info");
+	colunaEdit.appendChild(btnEdit);
+	linha.appendChild(colunaEdit);
+
+	let colunaDelete = document.createElement("td");
+	let btnDelete = document.createElement("button");
+	btnDelete.textContent = "Excluir";
+	btnDelete.classList.add("btn");
+	btnDelete.classList.add("btn-danger");
+	colunaDelete.appendChild(btnDelete);
+	linha.appendChild(colunaDelete);
+
+	const table = document.getElementeById("tableBody");
+	table.appendChild(linha);
+}
+
+loadTable();
