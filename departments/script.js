595c52c9d2b855887888fe2659ccd257b808d3fd
Walkiria <walkiria_tg@hotmail.com>
2021-08-16 20:00:33 -0300

    limpando o código de department (métodos das requisições em api.js da pasta service)

--- departments/script.js
+++ departments/script.js
@@ -1,71 +1,8 @@
-//fazendo um fetch dos dados passando nossa rota com await
+const route = "/departments/";
+
 let actualId = undefined;
-baseURL = "https://professor-allocation-walkiria.herokuapp.com/departments/";
 
-async function getData(){
-	const response = await fetch(baseURL);
-	const json = await response.json();
-	return json;
-}
-
-async function createData(name) {
-	fetch(baseURL, {
-    method: "POST",
-    body: JSON.stringify({name}),
-    headers: { "Content-Type": "application/json" },
-  }).then((response) => {
-    if (!response.ok) {
-      alert("houve um erro");
-    }
-
-    response.json().then((json) => {
-     createLine(json);
-    });
-  });
-}
-
-async function updateData(id, name) {
-	fetch(baseURL + id, {
-	    method: "PUT",
-	    body: JSON.stringify({ name: name }),
-	    headers: { "Content-Type": "application/json" },
-  	}).then((response) => {
-   		if (!response.ok) {
-     		 alert("Error!");
-    	}
-
-   		response.json().then((json) => {
-   		 refreshTable();     		
-    	});
- 	});
-}
-
-async function deleteData() {
-	fetch(baseURL + actualId, {
-    method: "DELETE",
-  }).then((response) => {
-    if (!response.ok) {
-      alert("houve um erro");
-    }
-   
-    refreshTable();
-  });
-}
-
-async function refreshTable() {
-	const table = document.getElementById("tableBody");
-	table.innerHTML = '';
-
-	loadTable();
-}
-
-async function loadTable(){
-	const data = await getData();
-	
-	for (const item of data){
-		createLine(item);
-	}
-}
+const table = document.getElementById("tableBody");
 
 async function createLine(dep) {
 	let linha = document.createElement("tr");
@@ -80,7 +17,7 @@ async function createLine(dep) {
 	btnEdit.classList.add("btn");
 	btnEdit.classList.add("btn-info");
 
-	btnEdit.addEventListener("click", () => updateDepartment(dep));
+	btnEdit.addEventListener("click", () => btnUpdate_click(dep));
 
 	colunaEdit.appendChild(btnEdit);
 	linha.appendChild(colunaEdit);
@@ -91,7 +28,7 @@ async function createLine(dep) {
 	btnDelete.classList.add("btn");
 	btnDelete.classList.add("btn-danger");
 
-	btnDelete.addEventListener("click", () => deleteDepartment(dep));
+	btnDelete.addEventListener("click", () => btnDelete_click(dep));
 
 	colunaDelete.appendChild(btnDelete);
 	linha.appendChild(colunaDelete);
@@ -100,27 +37,30 @@ async function createLine(dep) {
 	table.appendChild(linha);
 }
 
-loadTable();
+async function refreshTable() {	
+	table.innerHTML = '';
 
-function createDepartment() {
-	const department = document.getElementById("txtName").value;	
-
-	if (!department){
-		alert("The department name is required!");
-	}
-
-	if (!actualId) {
-		createData(department);
-	} else {
-		updateData(actualId, department);
-	}
+	loadTable();
 }
 
-function updateDepartment(dep){
+async function loadTable(){
+	let data = await getData(route);
+	
+	for (let item of data){
+		createLine(item);
+	}
+}
+// evento disparado quando aperta em criar um elemento
+function btnAdd_click() {
+	document.getElementById("txtName").value = "";
+	const title = document.getElementById("modalCreateTitle");
+	title.textContent = "Create Department";
+	actualId = undefined;
+}
+// evento disparado quando aperta em editar um elemento
+function btnUpdate_click(dep) {
 	const title = document.getElementById("modalCreateTitle");
 	title.textContent = "Update Department";
-
-	/*const department = document.getElementById("txtName").value; */
 
 	document.getElementById("txtName").value = dep.name;
 	actualId = dep.id;
@@ -128,8 +68,8 @@ function updateDepartment(dep){
 	var myModal = new bootstrap.Modal(document.getElementById('modalCreate'))
 	myModal.show();
 }
-
-function deleteDepartment(dep) {
+// evento disparado quando aperta em deletar um elemento
+function btnDelete_click(dep) {
 	actualId = dep.id;	
 
 	const txtDepartment = document.getElementById('txtDeleteDepartment');
@@ -138,21 +78,47 @@ function deleteDepartment(dep) {
 	var myModal = new bootstrap.Modal(document.getElementById('modalDelete'))
 	myModal.show();
 }
+// evento disparado quando aperta no botão que confirma a criação/atualização do novo elemento
+async function applyAddDepartament() {
+	const department = document.getElementById("txtName").value;
 
-const confirmSave = document.getElementById("btnModalCreate");
-confirmSave.addEventListener("click", createDepartment);
+	let result;
 
-const confirmDelete = document.getElementById("btnModalDelete");
-confirmDelete.addEventListener("click", deleteData);
+	if (!department){
+		alert("The department name is required!");
+		return;
+	}
 
+	if (!actualId) {
+		createData(department);
+	} else {
+		updateData(actualId, department);
+	}
+
+	if (result) {
+    refreshTable();
+  }
+}
+// evento disparado quando aperta no botão que confirma a exclusão do elemento
+async function applyRemoveDepartament() {
+	const result = await deleteData(route + actualId);
+
+  if (result) {
+    refreshTable();
+  }
+
+}
+//informando o evento do botao de adicionar
 const btnAdd = document.getElementById("btnAdd");
-btnAdd.addEventListener("click", () => {
-	document.getElementById("txtName").value = "";
-	
-	const title = document.getElementById("modalCreateTitle");
-	title.textContent = "Create Department";
-	actualId = undefined;
-});
+btnAdd.addEventListener("click", btnAdd_click);
 
-console.log(btnAdd);
+//informando o evento pegando do botao de confirmação
+const confirmSave = document.getElementById("btnModalCreate");
+confirmSave.addEventListener("click", applyAddDepartament);
 
+// pegando o botao de confirmar a remocao e informando seu evento
+const confirmDelete = document.getElementById("btnModalDelete");
+confirmDelete.addEventListener("click", applyRemoveDepartament);
+
+// chamando o método de carregar a tabela para exibir na tela
+loadTable();
