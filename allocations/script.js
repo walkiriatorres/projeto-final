34ebfdcfbfad90a14188ccabe910088162104588
Walkiria <walkiria_tg@hotmail.com>
2021-08-25 01:22:58 -0300

    no message

--- allocations/script.js
+++ allocations/script.js
@@ -132,9 +132,11 @@ async function applyAddAllocation(){
 	}
 	if (startHour == endHour) {
 		alert("Não é possível cadastrar aula com horário inicial e final iguais");
+		return;
 	}
 	if (startHour > endHour) {
 		alert("Não foi possível cadastrar aula, horário inicial selecionado foi maior que o horário final.");
+		return;
 	}
 
 	const data = {
