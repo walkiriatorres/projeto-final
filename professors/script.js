09287defc6ef14486accd1cda63142a587b40ef6
Walkiria <walkiria_tg@hotmail.com>
2021-08-26 11:40:30 -0300

    validação de cpf apenas emitindo alerta

--- professors/script.js
+++ professors/script.js
@@ -162,3 +162,53 @@ async function loadSelectDepartmentId() {
 loadSelectDepartmentId();
 
 loadTable();
+
+function validarCPF(){
+   var cpf = document.getElementById("txtCPF");
+      
+   if(cpf.length != 11 || cpf == "00000000000" || 
+      cpf == "11111111111" ||
+      cpf == "22222222222" || cpf == "33333333333" || 
+      cpf == "44444444444" ||
+      cpf == "55555555555" || cpf == "66666666666" || 
+      cpf == "77777777777" ||
+      cpf == "88888888888" || cpf == "99999999999"){
+      alert("CPF inválido. Tente novamente.");
+      return false;
+   }
+  
+   soma = 0;
+   for(i = 0; i < 9; i++){
+     soma += parseInt(cpf.charAt(i)) * (10 - i);
+   }   
+ 
+   resto = 11 - (soma % 11);
+    
+   if(resto == 10 || resto == 11){
+     resto = 0;
+   }
+    
+   if(resto != parseInt(cpf.charAt(9))){
+     alert("CPF inválido. Tente novamente.");
+     return false;
+   }
+ 
+   soma = 0;
+   for(i = 0; i < 10; i ++){
+     soma += parseInt(cpf.charAt(i)) * (11 - i);
+   }   
+ 
+   resto = 11 - (soma % 11);
+   if(resto == 10 || resto == 11){
+     resto = 0;
+   }
+ 
+   if(resto != parseInt(cpf.charAt(10))){
+     alert("CPF inválido. Tente novamente.");
+     return false;
+   }
+ 
+   alert("CPF válido. Muito obrigado."); 
+   return true;
+}
+
