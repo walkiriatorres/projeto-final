6efdd9d6116820132656f54e41616cacfd462a0d
Walkiria <walkiria_tg@hotmail.com>
2021-08-26 11:34:32 -0300

    limitando cpf em 11 caracteres apenas numeros

--- professors/script.js
+++ professors/script.js
@@ -69,49 +69,6 @@ function btnAdd_click() {
 	title.textContent = "Create Professor";
 	actualId = undefined;
 
-
-
-	
-
-	//validando cpf:
-	let value_cpf = document.getElementById('txtCPF');
-
-	value_cpf.addEventListener("blur", function(e) {
-	     //Remove tudo o que não é dígito
-	     let validar_cpf = this.value.replace( /\D/g , "");
-
-	     //verificação da quantidade números
-	     if (validar_cpf.length==11){
-
-	     // verificação de CPF valido
-	      var Soma;
-	      var Resto;
-
-	      Soma = 0;
-	      for (i=1; i<=9; i++) Soma = Soma + parseInt(validar_cpf.substring(i-1, i)) * (11 - i);
-	         Resto = (Soma * 10) % 11;
-
-	      if ((Resto == 10) || (Resto == 11))  Resto = 0;
-	      if (Resto != parseInt(validar_cpf.substring(9, 10)) ) return alert("CPF Inválido!");;
-
-	      Soma = 0;
-	      for (i = 1; i <= 10; i++) Soma = Soma + parseInt(validar_cpf.substring(i-1, i)) * (12 - i);
-	      Resto = (Soma * 10) % 11;
-
-	      if ((Resto == 10) || (Resto == 11))  Resto = 0;
-	      if (Resto != parseInt(validar_cpf.substring(10, 11) ) ) return alert("CPF Inválido!");;
-
-	      //mascarando cpf
-	      cpf_final = validar_cpf.replace( /(\d{3})(\d)/ , "$1.$2");
-	      cpf_final = cpf_final.replace( /(\d{3})(\d)/ , "$1.$2");
-	      cpf_final = cpf_final.replace(/(\d{3})(\d{1,2})$/ , "$1-$2");
-	      document.getElementById('txtCPF').value = cpf_final;
-
-	     } else {
-	       alert("CPF Inválido! É esperado 11 dígitos numéricos.")
-	     }
-	 });
-
 }
 
 function btnUpdate_click(professor){
@@ -123,47 +80,6 @@ function btnUpdate_click(professor){
 	document.getElementById("selectDepartmentId").value = professor.department.id;
 
 	actualId = professor.id;
-
-	//validando cpf:
-	let value_cpf = document.getElementById('txtCPF');
-
-	value_cpf.addEventListener("blur", function(e) {
-	     //Remove tudo o que não é dígito
-	     let validar_cpf = this.value.replace( /\D/g , "");
-
-	     //verificação da quantidade números
-	     if (validar_cpf.length==11){
-
-	     // verificação de CPF valido
-	      var Soma;
-	      var Resto;
-
-	      Soma = 0;
-	      for (i=1; i<=9; i++) Soma = Soma + parseInt(validar_cpf.substring(i-1, i)) * (11 - i);
-	         Resto = (Soma * 10) % 11;
-
-	      if ((Resto == 10) || (Resto == 11))  Resto = 0;
-	      if (Resto != parseInt(validar_cpf.substring(9, 10)) ) return alert("CPF Inválido!");;
-
-	      Soma = 0;
-	      for (i = 1; i <= 10; i++) Soma = Soma + parseInt(validar_cpf.substring(i-1, i)) * (12 - i);
-	      Resto = (Soma * 10) % 11;
-
-	      if ((Resto == 10) || (Resto == 11))  Resto = 0;
-	      if (Resto != parseInt(validar_cpf.substring(10, 11) ) ) return alert("CPF Inválido!");;
-
-	      //formatação final
-	      cpf_final = validar_cpf.replace( /(\d{3})(\d)/ , "$1.$2");
-	      cpf_final = cpf_final.replace( /(\d{3})(\d)/ , "$1.$2");
-	      cpf_final = cpf_final.replace(/(\d{3})(\d{1,2})$/ , "$1-$2");
-	      document.getElementById('txtCPF').value = cpf_final;
-
-	     } else {
-	       alert("CPF Inválido! É esperado 11 dígitos numéricos.")
-	     }   
-
-	 })
-
 
 	var myModal = new bootstrap.Modal(document.getElementById('modalCreate'));
 	myModal.show();
